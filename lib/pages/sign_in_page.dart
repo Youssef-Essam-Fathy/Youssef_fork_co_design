377fc74d99a2bd643c5c3f10dc2187ee6718fc19
Youssef <name@email.com>
2022-09-18 02:20:15 +0200

    Add files to git

---
+++ lib/pages/sign_in_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class signInPage extends StatefulWidget {
+  const signInPage({Key? key}) : super(key: key);
+
+  @override
+  State<signInPage> createState() => _signInPageState();
+}
+
+class _signInPageState extends State<signInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children:[
+
+        ],
+      ),
+    );
+  }
+}
